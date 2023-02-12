57d50ea7076e790e5c071c2ad304494cb90b198d
JoeSH2 <foremansh2@mail.ru>
2023-02-12 18:59:03 +0300

    Commit v1

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,10 @@
 type Mode = Record<string, boolean | string>;
 
-export const classNames = (className: string, mode: Mode = {}, additional: string[] = []): string => [
+export const classNames = (
+  className: string,
+  mode: Mode = {},
+  additional: string[] = [],
+): string => [
   className,
   ...Object.entries(mode)
     .filter(([_, key]) => Boolean(key))
