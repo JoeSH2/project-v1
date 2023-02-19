1e10686556335702b2abd32f92587aabef4299dc
JoeSH2 <foremansh2@mail.ru>
2023-02-19 14:47:02 +0300

    add github actions YML

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,23 +6,32 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-export const buildPlugins = ({
+export function buildPlugins2({
   paths,
   isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HTMLWebpackPlugin({
-    template: paths.html,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  isDev && new ReactRefreshPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugin = [
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugin.push(
+      new ReactRefreshPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    );
+  }
+
+  return plugin;
+}
